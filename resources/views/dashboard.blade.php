83a03a173f5274b0c5be972e5c06f37e72aa079c
Alex Chasi <alexchasi17@gmail.com>
2023-01-10 01:23:15 -0500

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                    Bienvenidos este es el blog de Alex..
+
+                    Hola mi nombre es: Alex Chasi les doy la bienvenida a esta prueba de usuario logueado con el correo y la contraseña , la contraseña es minimo 8 caracteres al registrar un Usuario
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
